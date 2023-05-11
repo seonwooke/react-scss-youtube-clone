889725c305a7f78fc606b219ae6e5cb1700f2d51
seonwooke <seonookkim@gmail.com>
2023-05-11 14:21:45 +0900

    :art: Initial Setting
    Initial Setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes, Outlet } from 'react-router-dom';
+import VideoPage from './pages/VideoPage';
+import NavigationBar from './components/NavigationBar';
+import SideBar from './components/SideBar';
+import MainPage from './pages/MainPage';
+import SearchedVidoesPage from './pages/SearchedVideosPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Layout = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavigationBar />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
     </>
-  )
+  );
+};
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path='/results/:input' element={<SearchedVidoesPage />} />
+          <Route path='/video/:videoId' element={<VideoPage />} />
+        </Route>
+      </Routes>
+    </React.Fragment >
+  );
 }
 
-export default App
+export default App;
+
